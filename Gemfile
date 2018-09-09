e4764efe53242dd9d7fc8db50762925c0350c1cf
[kousy] <[koseiide@gmail.com]>
2018-09-09 10:30:22 +0900

    add pry

--- Gemfile
+++ Gemfile
@@ -1,3 +1,5 @@
 source "https://rubygems.org"
 
 gem 'sinatra'
+gem 'rss'
+gem 'pry'
--- app.rb
+++ app.rb
@@ -1,8 +1,14 @@
 require 'sinatra'
 require 'rss'
+require 'pry'
+
 get '/' do
   @rss1 = RSS::Parser.parse "http://shgam.hatenadiary.jp/rss"
   @rss2 = RSS::Parser.parse "https://kousy.hatenablog.com/rss"
   @hello = "app.rbから文字を表示"
   erb :index
+end
+
+get '/create' do
+  erb :create
 end
